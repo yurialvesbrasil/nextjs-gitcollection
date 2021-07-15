d6cb475adbcae2eb2d62302f2bc419ca07195ef6
yurialvesbrasil <yurihotmail@hotmail.com>
2021-07-15 16:31:32 -0300

    Style dos itens listados na pagina do dasboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Title, Form } from './style';
+import { Title, Form, Repos } from './style';
 import logo from '../../assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
 
 export const Dasboard: React.FC = () => {
   return (
     <>
       <img src={logo} alt="Logo da aplicação" />
       <Title>Catálogo de Repositórios do Github</Title>
+
       <Form>
         <input placeholder="username/repository_name" />
         <button type="submit">Buscar</button>
       </Form>
+
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://avatars.githubusercontent.com/u/6756311?v=4"
+            alt="Repositorio"
+          />
+          <div>
+            <strong>yurialvesbrasil/mini-curso</strong>
+            <p>Descrição completa do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
     </>
   );
 };
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,38 +3,42 @@ import imgBackground from '../assets/background.svg';
 
 export const GlobalStyle = createGlobalStyle`
   * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
 
   html {
-      @media (max-width: 1080px){
-          fonte-size: 93.75%
-      }
+    @media (max-width: 1080px){
+        fonte-size: 93.75%
+    }
 
-      @media (max-width: 720px){
-          fonte-size: 87.5%
-      }
+    @media (max-width: 720px){
+        fonte-size: 87.5%
+    }
   }
 
   body{
-      background: #f0f0f5 url(${imgBackground}) no-repeat 70% top;
-      -webkit-font-smoothing: antialiased;
+    background: #f0f0f5 url(${imgBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, select, button {
+    font: 400 1rem "Roboto", sans-serif;
   }
 
   #root{
-      max-width: 960px;
-      margin: 0 auto;
-      padding: 2.5rem 1.25rem;
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 2.5rem 1.25rem;
   }
 
   button {
-      cursor: pointer;
+    cursor: pointer;
   }
 
   a {
-      color: inherit;
-      text-decoration: none;
+    color: inherit;
+    text-decoration: none;
   }
 `;
