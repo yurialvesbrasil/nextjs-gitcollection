f1d1a984b1a6b41c11bc8fd0a2dca11ed433bd33
yurialvesbrasil <yurihotmail@hotmail.com>
2021-07-15 15:22:08 -0300

    input e button da page - Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Title } from './style';
+import { Title, Form } from './style';
 import logo from '../../assets/logo.svg';
 
 export const Dasboard: React.FC = () => {
   return (
     <>
       <img src={logo} alt="Logo da aplicação" />
-      <Title>Dasboard</Title>
+      <Title>Catálogo de Repositórios do Github</Title>
+      <Form>
+        <input placeholder="username/repository_name" />
+        <button type="submit">Buscar</button>
+      </Form>
     </>
   );
 };
